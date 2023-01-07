c9236c772f14b73f9a7ce5ed509b1605c299d83f
Vladimir Yakovlev <vladimiryakovlev25@gmail.com>
2023-01-07 16:56:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов ?', ''),
+      b = +prompt('На сколько оцениваете его ?', ''),
+      c = prompt('Один из последних просмотренных фильмов ?', ''),
+      d = +prompt('На сколько оцениваете его ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
